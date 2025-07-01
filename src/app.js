647eaefcfcd02d5d12c56c0d0cd2fde9b3831002
David Betancourt <dmarquez.develop@gmail.com>
2025-07-01 02:06:01 +0000

    added number&symbol missing colors

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardType = ['♦', '♥', '♠', '♣'];
+  let cardNumber = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+  let randomCardType = cardType[Math.floor(Math.random()*cardType.length)];
+  let randomCardNumber = cardNumber[Math.floor(Math.random()*cardNumber.length)]; 
+  document.getElementById('number').innerHTML = randomCardNumber;
+  document.getElementById('symbol1').innerHTML = randomCardType;
+  document.getElementById('symbol2').innerHTML = randomCardType;
+  if (randomCardType == '♦') {
+    document.getElementById('symbol1').style.color = 'red'
+  }
 };
+
+// Pseudocode
+// asignarle un color a number
+// asignarle un color a cardtype
