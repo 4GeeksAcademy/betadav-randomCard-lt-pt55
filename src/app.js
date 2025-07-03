8a51ae72ad10c81b72bc224e94016dd340949a72
David Betancourt <dmarquez.develop@gmail.com>
2025-07-03 02:43:35 +0000

    added inputs and option to change size

--- src/app.js
+++ src/app.js
@@ -1,23 +1,46 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let cardType = ['♦', '♥', '♠', '♣'];
-  let cardNumber = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
-  let randomCardType = cardType[Math.floor(Math.random()*cardType.length)];
-  let randomCardNumber = cardNumber[Math.floor(Math.random()*cardNumber.length)]; 
-  let color = (randomCardType === '♦' || randomCardType === '♥') ? 'red' : 'black';
+  
+  function changeCard() {
+    let cardType = ['♦', '♥', '♠', '♣'];
+    let cardNumber = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+    let randomCardType = cardType[Math.floor(Math.random()*cardType.length)];
+    let randomCardNumber = cardNumber[Math.floor(Math.random()*cardNumber.length)]; 
+    let color = (randomCardType === '♦' || randomCardType === '♥') ? 'red' : 'black';
 
-  document.getElementById('number').innerHTML = randomCardNumber;
-  document.getElementById('number').style.color = color;
+    document.getElementById('number').innerHTML = randomCardNumber;
+    document.getElementById('number').style.color = color;
 
-  document.getElementById('symbol1').innerHTML = randomCardType;
-  document.getElementById('symbol1').style.color = color;
+    document.getElementById('symbol1').innerHTML = randomCardType;
+    document.getElementById('symbol1').style.color = color;
 
-  document.getElementById('symbol2').innerHTML = randomCardType;
-  document.getElementById('symbol2').style.color = color;
+    document.getElementById('symbol2').innerHTML = randomCardType;
+    document.getElementById('symbol2').style.color = color;
+
+    document.getElementById('btnClick').addEventListener("click", changeCard);
+    
+    // update card every 10s
+    // setInterval(changeCard, 10000);
+
+  };
+  changeCard();
+
 };
+
+  function changeSize() {
+    let ancho = document.getElementById('ancho').value ;
+    let alto = document.getElementById('alto').value;
+    document.querySelector('.card').style.width = ancho + 'px';
+    document.querySelector('.card').style.height = alto + 'px';
+    document.getElementById('btn2').addEventListener("click", changeSize);
+  };
+  changeSize();
+
+
+  
+
